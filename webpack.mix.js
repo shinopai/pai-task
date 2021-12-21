8d840bb72443d8626e4530400f3428f0b971a371
shinopai <harbor.shinozaki@gmail.com>
2021-12-21 14:17:13 +0000

    add vue.js]

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,5 @@
 const mix = require('laravel-mix');
+require("laravel-mix-vue3");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
+mix.vue3('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
